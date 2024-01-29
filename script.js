8c1cef4db12e118644b59fc1c4533a64bd5ce380
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-29 07:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function twoSum (){
+	const number1 = document.getElementById("n1");
+	const number2 = document.getElementById("n2");
+     const Sum = document.getElementById("Sum");
+	const sumBtn = document.getElementById("sum_btn");
+
+	sumBtn.addEventListener("click", function(){
+		const num1 = Number(number1.value);
+		const num2 = Number(number2.value);
+		const total = num1 + num2 ;
+		
+		 Sum.textContent = total;
+		
+	});
+}
+	twoSum();
+
+
+
+
+
+
+
+
+
+	
+	
